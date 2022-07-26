c2d39b8df88723fe08be4b147a7488f274b97ddb
Xingjian Leng <xingjianleng@gmail.com>
2022-07-26 17:38:04 +1000

    initial commit

---
+++ assignment1/query_boolean.py
@@ -0,0 +1,65 @@
+from collections import defaultdict
+import pickle
+import os
+import numpy as np
+
+from string_processing import *
+
+
+def intersect_query(doc_list1, doc_list2):
+    # TODO: you might like to use a function like this 
+    # in your run_boolean_query implementation
+    # for full marks this should be the O(n + m) intersection algorithm for sorted lists
+    # using data structures such as sets or dictionaries in this function will not score full marks
+    return res
+
+def union_query(doc_list1, doc_list2):
+    # TODO: you might like to use a function like this 
+    # in your run_boolean_query implementation
+    # for full marks this should be the O(n + m) union algorithm for sorted lists
+    # using data structures such as sets or dictionaries in this function will not score full marks
+    return res
+
+def run_boolean_query(query, index):
+    """Runs a boolean query using the index.
+
+    Args:
+        query (str): boolean query string
+        index (dict(str : list(tuple(int, int)))): The index aka dictionary of posting lists
+
+    Returns:
+        list(int): a list of doc_ids which are relevant to the query
+    """
+    # TODO: implement this function
+
+    return relevant_docs
+
+
+# load the stored index
+(index, doc_freq, doc_ids, num_docs) = pickle.load(open("stored_index.pik", "rb"))
+
+print("Index length:", len(index))
+if len(index) != 906290:
+    print("Warning: the length of the index looks wrong.")
+    print("Make sure you are using `process_tokens_original` when you build the index.")
+    raise Exception()
+
+# the list of queries asked for in the assignment text
+queries = [
+    "Welcoming",
+    "Australasia OR logistic",
+    "heart AND warm",
+    "global AND space AND wildlife",
+    "engine OR origin AND record AND wireless",
+    "placement AND sensor OR max AND speed"
+]
+
+# run each of the queries and print the result
+ids_to_doc = {v:k for k, v in doc_ids.items()}
+for q in queries:
+    res = run_boolean_query(q, index)
+    res.sort(key=lambda x: ids_to_doc[x])
+    print(q)
+    for r in res:
+        print(ids_to_doc[r])
+
