08f0fcb4e34678efc8300154f502149652aacee4
Xingjian Leng <xingjianleng@gmail.com>
2022-10-14 22:22:03 +1100

    Except crossvalidation

--- assignment3/Q3/rel_extract.py
+++ assignment3/Q3/rel_extract.py
@@ -1,20 +1,16 @@
 from itertools import product
 import json
 import pandas as pd
-import numpy as np
 import spacy
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 import tqdm
 
 
 # read in the data
-# train_data = json.load(open("sents_parsed_train.json", "r"))
-# test_data = json.load(open("sents_parsed_test.json", "r"))
-train_data = json.load(open("train.json", "r"))
-test_data = json.load(open("val.json", "r"))
+train_data = json.load(open("sents_parsed_train.json", "r"))
+test_data = json.load(open("sents_parsed_test.json", "r"))
 
 with open("country_names.txt", "r") as f:
     countries = f.read()
@@ -54,6 +50,7 @@ def print_example(data, index):
     else:
         print("Test examples do not have ground truth relations.")
 
+
 def write_output_file(relations, filename = "q3.csv"):
     """The list of relations into a csv file for the evaluation script
 
@@ -81,7 +78,7 @@ print("Testing example:")
 # truth relation
 print_example(test_data, 2)
 
-#TODO: build a training/validation/testing pipeline for relation extraction
+# TODO: build a training/validation/testing pipeline for relation extraction
 #       then write the list of relations extracted from the *test set* to "q3.csv"
 #       using the write_output_file function.
 
@@ -104,31 +101,27 @@ def sentence_processing(sent, start, end):
 def train_preprocess(data_dict):
     feature_sents = []
     labels = []
-    distance = []
     nationality = '/people/person/nationality'
 
     for sent in tqdm.tqdm(data_dict):
         # extract relation indices
         relation_a, relation_b = sent["relation"]["a"], sent["relation"]["b"]
-
-        for entity in sent["entities"]:
-            # range from first proper noun to last proper noun
-            entity_tokens = sent["tokens"][entity["start"]: entity["end"]]
-            if entity_tokens == relation_a:
-                entity_idx_a = entity["start"], entity["end"]
-            if entity_tokens == relation_b:
-                entity_idx_b = entity["start"], entity["end"]
-
-        # NOTE: inspired from online resources, see report
-        # we allow the relation word to extend the relation word with length 3
+        idx_a = sent["relation"]["a_start"], sent["relation"]["a_start"] + len(relation_a)
+        idx_b = sent["relation"]["b_start"], sent["relation"]["b_start"] + len(relation_b)
+        
         # Features: words in between entities, entities themselves, words in particular positions
-        start = max(min(entity_idx_a[0], entity_idx_b[0]) - 3, 0)
-        end = min(max(entity_idx_a[1], entity_idx_b[1]) + 3, len(sent["tokens"]))
+        start = max(min(idx_a[0], idx_b[0]) - 1, 0)
+        end = min(max(idx_a[1], idx_b[1]) + 1, len(sent["tokens"]))
 
-        # preprocessing features
+        # preprocessing bag of word
         processed_sequence = sentence_processing(sent, start, end)
+        # Features: headword and the concatenation of them
+        head_a = relation_a[0]
+        head_b = relation_b[0]
+        concatenation = f"{head_a}-{head_b}"
+        processed_sequence.extend([head_a, head_b, concatenation])
+
         feature_sents.append(" ".join(processed_sequence))
-        distance.append(end - start)
 
         # preprocessing labels
         if sent["relation"]["relation"] == nationality:
@@ -136,99 +129,93 @@ def train_preprocess(data_dict):
         else:
             labels.append(0)
     
-    cv = CountVectorizer()
-    train_features = cv.fit_transform(feature_sents).toarray()
-    embedded_features = np.hstack((train_features, np.array(distance).reshape(-1, 1)))
+    cv = CountVectorizer(ngram_range=(1, 2))
+    train_features = cv.fit_transform(feature_sents)
 
-    return embedded_features, cv, labels
+    return train_features, cv, labels
 
 
 def test_preprocess(data_dict, cv):
-    indices_original_sent = []
     feature_sents = []
     entities = []
-    distance = []
+    entity_distance = []
 
-    for i, sent in enumerate(tqdm.tqdm(data_dict)):
+    for sent in tqdm.tqdm(data_dict):
         # we only extract GPE and PERSON as they are the only chance for nationality
         gpes_indices = []
         persons_indices = []
         for entity in sent["entities"]:
-            x = sent["tokens"][entity["start"]: entity["end"]]
-            if entity["label"] == "GPE" and " ".join(sent["tokens"][entity["start"]: entity["end"]]) in countries:
+            # could be the case that it have stopwords so doesn't belong to countries
+            if entity["label"] == "GPE":
                 # check if the entity is in country list
-                gpes_indices.append((entity["start"], entity["end"]))
+                entity_word = " ".join([sent["tokens"][index]
+                        for index in range(entity["start"], entity["end"]) if not sent["isstop"][index]])
+                if entity_word in countries:
+                    gpes_indices.append((entity["start"], entity["end"]))
             elif entity["label"] == "PERSON":
+                # person names
                 persons_indices.append((entity["start"], entity["end"]))
 
         entity_indices_combinations = product(persons_indices, gpes_indices)
         
         # extract features for each entity pairs in the given list
-        for a_idx, b_idx in entity_indices_combinations:
+        for idx_a, idx_b in entity_indices_combinations:
             # allow a extension of window 3
-            start = max(min(a_idx[0], b_idx[0]) - 3, 0)
-            end = min(max(a_idx[1], b_idx[1]) + 3, len(sent["tokens"]))
+            start = max(min(idx_a[0], idx_b[0]) - 1, 0)
+            end = min(max(idx_a[1], idx_b[1]) + 1, len(sent["tokens"]))
 
             # preprocessing features
             processed_sequence = sentence_processing(sent, start, end)
+            # we also add headword and the concatenation of them
+            head_a = sent["tokens"][idx_a[0]]
+            head_b = sent["tokens"][idx_b[0]]
+            concatenation = f"{head_a}-{head_b}"
 
-            # append the corresponding sentence index
-            indices_original_sent.append(i)
+            processed_sequence.extend([head_a, head_b, concatenation])
+
             # append features to the list
             feature_sents.append(" ".join(processed_sequence))
-            # append the entity words used
-            entities.append((sent["tokens"][a_idx[0]: a_idx[1]], sent["tokens"][b_idx[0]: b_idx[1]]))
-            # add distance (# of words) between entities
-            distance.append(end - start)
+            # append the entity words used, we need to skip stopwords in entities
+            entities.append(
+                (
+                    " ".join([sent["tokens"][index]
+                        for index in range(idx_a[0], idx_a[1]) if not sent["isstop"][index]]),
+                    " ".join([sent["tokens"][index]
+                        for index in range(idx_b[0], idx_b[1]) if not sent["isstop"][index]])
+                )
+            )
+            # append distance between entities
+            entity_distance.append(end - start)
 
-    features = cv.transform(feature_sents).toarray()
-    embedded_features = np.hstack((features, np.array(distance).reshape(-1, 1)))
+    features = cv.transform(feature_sents)
 
-    return indices_original_sent, embedded_features, entities
+    return features, entities, entity_distance
 
 
-def test_postprocess(predictions, entities):
+def test_postprocess(prediction_prob, entities, entity_distance):
     relations = []
-    for i, prediction in enumerate(predictions):
-        if prediction == 1:
-            relations.append((" ".join(entities[i][0]), " ".join(entities[i][1])))
+    name_probs = {}
+    for i, prob in enumerate(prediction_prob):
+        name = entities[i][0]
+        confidence = prob[1]
+        if (name not in name_probs or confidence > name_probs[name][0]) and confidence > 0.5 and entity_distance[i] < 30:
+            # if name not in dict or higher confidence, update attribute
+            name_probs[name] = (confidence, entities[i][1])
+    for key, value in name_probs.items():
+        relations.append((key, value[1]))
     write_output_file(relations=relations)
 
 
 train_x, cv, train_y = train_preprocess(train_data)
-train_x, val_x, train_y, val_y = train_test_split(train_x, train_y, test_size=0.2)
+train_x, val_x, train_y, val_y = train_test_split(train_x, train_y, test_size=0.15)
 
-lr = LogisticRegression(max_iter=500, class_weight="balanced")
+lr = LogisticRegression(max_iter=3000, class_weight="balanced")
 lr.fit(train_x, train_y)
-
-# training data
-pred_val = lr.predict(train_x)
-precision = precision_score(train_y, pred_val)
-acu = accuracy_score(train_y, pred_val)
-recall = recall_score(train_y, pred_val, average="macro")
-score = f1_score(train_y, pred_val)
-print("precision %f "%precision)
-print("recall %f "%recall)
-print("auccuacy %f "%acu)
-print("score %f "%score)
-print()
-
-# validation
-pred_val = lr.predict(val_x)
-precision = precision_score(val_y, pred_val)
-acu = accuracy_score(val_y, pred_val)
-recall = recall_score(val_y, pred_val, average="macro")
-score = f1_score(val_y, pred_val)
-print("precision %f "%precision)
-print("recall %f "%recall)
-print("auccuacy %f "%acu)
-print("score %f "%score)
-
 
 # Example only: write out some relations to the output file
 # normally you would use the list of relations output by your model
 # as an example we have hard coded some relations from the training set to write to the output file
 # TODO: remove this and write out the relations you extracted (obviously don't hard code them)
-test_sent_indices, test_features, entities = test_preprocess(test_data, cv)
-test_predictions = lr.predict(test_features)
-test_postprocess(test_predictions, entities)
+test_features, entities, entity_distance = test_preprocess(test_data, cv)
+prediction_prob = lr.predict_proba(test_features)
+test_postprocess(prediction_prob, entities, entity_distance)
